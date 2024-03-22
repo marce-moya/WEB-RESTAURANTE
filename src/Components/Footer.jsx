d922b0ab9eadf3ca558ef8c6f8e1aaaf2bed50fe
marce-moya <marcelamoya@gmail>
2024-03-21 22:39:16 -0300

    agregar iconos RRSS al footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,6 @@
+import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -6,10 +8,14 @@ const Footer = () => {
       <Container>
         <Row>
           <Col md={6}>
-            <h6>Información de contacto</h6>
-            <p>Dirección: Calle Principal #123</p>
-            <p>Teléfono: 555-1234-5678</p>
-            <p>Email: empanadasdelacasa@gmail.com</p>
+          <h6>Información de contacto</h6>
+            <p style={{ textAlign: 'justify', marginBottom: '5px' }}>Dirección: Calle Principal #123</p>
+            <p style={{ textAlign: 'justify', marginBottom: '5px' }}>Teléfono: 555-1234-5678</p>
+            <p style={{ textAlign: 'justify', marginBottom: '5px' }}>Email: empanadasdelacasa@gmail.com</p>
+            <div style={{ display: 'flex', gap: '10px', marginTop: '10px' }}>
+              <a href="https://www.facebook.com/home.php" target="_blank" rel="noopener noreferrer" style={{ color: 'orange' }}><FaFacebook /></a>
+              <a href="https://www.instagram.com/?hl=es-es" target="_blank" rel="noopener noreferrer" style={{ color: 'orange' }}><FaInstagram /></a>
+            </div>
           </Col>
           <Col md={6}>
             <h6>Enlaces útiles</h6>
@@ -27,49 +33,3 @@ const Footer = () => {
 }
 
 export default Footer
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-// import { Container, Row, Col } from 'react-bootstrap'
-// import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa'
-
-// const Footer = () => {
-//   return (
-//     <footer className="bg-dark text-light py-4" style={{ fontFamily: 'Arial, sans-serif', fontSize: '14px' }}>
-//       <Container>
-//         <Row>
-//           <Col md={6}>
-//           <h6>Información de contacto</h6>
-//             <p style={{ textAlign: 'justify' }}>Dirección: Calle Principal #123</p>
-//             <p style={{ textAlign: 'justify' }}>Teléfono: 555-1234-5678</p>
-//             <p style={{ textAlign: 'justify' }}>Email: empanadasdelacasa@gmail.com</p>
-//             <div style={{ display: 'flex', gap: '10px', marginTop: '10px' }}>
-//               <a href="https://www.facebook.com/home.php" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
-//               <a href="https://twitter.com/home" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
-//               <a href="https://www.instagram.com/?hl=es-es" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-//             </div>
-//           </Col>
-//           <Col md={6}>
-//             <h6>Enlaces útiles</h6>
-//             <ul className="list-unstyled">
-//               <li><a href="/"style={{ color: 'orange' }}>Home</a></li>
-//               <li><a href="/Blog"style={{ color: 'orange' }}>Blog</a></li>
-//               <li><a href="/Retirar"style={{ color: 'orange' }}>Retirar en local</a></li>
-//               <li><a href="/Contacto"style={{ color: 'orange' }}>Formulario de Contacto</a></li>
-//             </ul>
-//           </Col>
-//         </Row>
-//       </Container>
-//     </footer>
-//   );
-// }
-
-// export default Footer
